df1182cff9c3e1c07b5038a019801a048a3d8bc0
skoricky <pripisnov@bk.ru>
2017-07-13 00:29:32 +0300

    js, css, debug

--- js/ToDoTasks.js
+++ js/ToDoTasks.js
@@ -21,15 +21,14 @@ function getTasks() {
 
 function addNewTaskClick() {
     var title = $("#newTaskTitleText").val();
-    var taskStatus = "NEW";
     addNewTask(title, taskStatus);
 }
 
-function addNewTask(title, taskStatus) {
+function addNewTask(title) {
     var taskObj;
     taskObj = {
         title: "",
-        taskStatus: "",
+        taskStatus: "NEW",
         taskBody: "",
         contributors: "",
         dataBegin: "",
@@ -37,8 +36,8 @@ function addNewTask(title, taskStatus) {
         label: ""
     };
     taskObj.title = title;
-    taskObj.taskStatus = taskStatus;
     taskObj.label = (tasksArray.length).toString();
+
     tasksArray.push(taskObj);
     if(tasksArray.length > 1) {
         addNextTask(tasksArray.length - 1);
@@ -48,48 +47,71 @@ function addNewTask(title, taskStatus) {
     setLocalStorage();
 }
 
-function addFirstTask() {
-    $("#currentTask").show();
-    $("#currentTask").children("#currentTaskStatus").html(tasksArray[0].taskStatus);
-    $("#currentTask").children("#currentTaskTitle").html(tasksArray[0].title);
-    $("#currentTask").children("#currentTaskMore").html("More...");
-}
-
-function addNextTask(N) {
-    var taskCloneElement = $("#currentTask").clone(true);
+function currentTask(taskCloneElement, index) {
     taskCloneElement.prependTo("#tasksZone");
-    taskCloneElement.children("#currentTaskStatus").html(tasksArray[N].taskStatus);
-    taskCloneElement.children("#currentTaskTitle").html(tasksArray[N].title);
-    taskCloneElement.children("#currentTaskLabel").val(N);
+    taskCloneElement.children("#currentTaskStatus").html(tasksArray[index].taskStatus);
+    taskCloneElement.children("#currentTaskTitle").html(tasksArray[index].title);
+    taskCloneElement.children("#currentTaskLabel").val(index);
     taskCloneElement.children("#currentTaskMore").html("More...");
+    taskCloneElement.show();
 }
 
-function editTask(element) {
+function addFirstTask() {
+    remoteTasks();
+    var taskCloneElement = $("#currentTask").clone(true);
+    currentTask(taskCloneElement, 0);
+}
+
+function addNextTask(index) {
+    var taskElement =$("#currentTask");
+    var taskCloneElement = taskElement.clone(true);
+    currentTask(taskCloneElement, index);
+}
+
+function editTaskClick(element) {
     var index;
-    $('.currentTaskMore:hover').each(function () {
+    $('.currentTaskMore').each(function () {
         if(element === this) {
             index = (Number)($(this).parent().children("#currentTaskLabel").val());
 
-            $(".taskModal").show();
-            var modal = $(".taskModal");
-            modal.children("#taskModalLabel").val(index.toString());
-            modal.children("#taskModalTitle").val(tasksArray[index].title);
-            modal.children("#taskModalBody").html(tasksArray[index].taskBody);
-            modal.children("#date").children("#dateIn").html(tasksArray[index].dataBegin);
-            modal.children("#date").children("#dateOut").html(tasksArray[index].dataDeathLine);
+
+            editTask(index);
         }
     })
+}
+
+function editTask(index) {
+    $(".taskModal").show();
+    var modal = $(".taskModal");
+    modal.children("#taskModalLabel").val(index.toString());
+    if(tasksArray.length > 0) {
+        modal.children("#taskModalTitle").val(tasksArray[index].title);
+        modal.children("#taskModalBody").html(tasksArray[index].taskBody);
+        modal.children("#date").children("#dateIn").html(tasksArray[index].dataBegin);
+        modal.children("#date").children("#dateOut").html(tasksArray[index].dataDeathLine);
+    } else {
+        var title = "task1";
+        addNewTask(title);
+        modal.children("#taskModalTitle").val(title);
+        modal.children("#taskModalBody").html("");
+        modal.children("#date").children("#dateIn").html("");
+        modal.children("#date").children("#dateOut").html("");
+    }
+
+}
+
+function saveTaskClick() {
+    saveTask();
 }
 
 function saveTask() {
     var modal = $(".taskModal");
     var index = (Number)(modal.children("#taskModalLabel").val());
-
     tasksArray[index].title = modal.children("#taskModalTitle").val();
     tasksArray[index].taskBody = modal.children("#taskModalBody").val();
     tasksArray[index].taskStatus = modal.children("#taskModalStatus").val();
-    // // tasksArray[index].dataBegin = modal.children("#date").children("#dateIn").value.toString();
-    // // tasksArray[index].dataDeathLine = modal.children("#date").children("#dateOut").value.toString();
+    tasksArray[index].dataBegin = modal.children("#date").children("#dateIn").val();
+    tasksArray[index].dataDeathLine = modal.children("#date").children("#dateOut").val();
     setLocalStorage();
     showEditTask(index);
     modal.hide();
@@ -97,7 +119,7 @@ function saveTask() {
 
 function showEditTask (index) {
     var showTask = tasksArray[index];
-    tasksArray.splice(index, 1)
+    tasksArray.splice(index, 1);
     tasksArray.push(showTask);
     labelSort();
     setLocalStorage();
@@ -105,21 +127,22 @@ function showEditTask (index) {
     getTasks();
 }
 
+$(document).on('click', '.currentTaskRemote', function (element) {
+    var index = (Number)($(this).parent().children("#currentTaskLabel").val());
+    index = tasksArray.indexOf(tasksArray.length === index.toString());
+    $(this).parent().remove();
+    if(tasksArray.length > 0) {
+        tasksArray.splice(index, 1);
+    } else {
 
-function remoteTask(element) {
-    var index;
-    $('.currentTaskRemote:hover').each(function () {
-        if(element === this) {
-            index = (Number)($(this).parent().children("#currentTaskLabel").val());
-            if(tasksArray.length > 0) {
-                tasksArray.splice(index, 1);
-            }
-            $(this).parent().hide();
-        }
-    });
-    labelSort();
+    }
+    if(tasksArray.length === 0) {
+        var taskCloneElement = $("#currentTask").clone();
+        taskCloneElement.prependTo("#tasksZone");
+        taskCloneElement.show();
+    }
     setLocalStorage();
-}
+});
 
 function labelSort () {
     for(var i = 0; i < tasksArray.length; i++) {
@@ -139,9 +162,12 @@ function getLocalStorage() {
 
 
     } else {
-        $("#currentTask").show();
+        var taskCloneElement = $("#currentTask").clone();
+        taskCloneElement.prependTo("#tasksZone");
+        taskCloneElement.show();
+        /*$("#currentTask").show();
         $("#currentTask").children("#currentTaskStatus").html("No task");
-        $("#currentTask").children("#currentTaskTitle").html("You don't have some task. Please add now...");
+        $("#currentTask").children("#currentTaskTitle").html("You don't have some task. Please add now...");*/
     }
 }
 
@@ -157,8 +183,17 @@ function setLocalStorage() {
 
     }
 }
+function remoteTasks() {
+    $(".currentTask").each(function () {
+        if($(this).is(":visible"))
+            $(this).remove();
+    });
+}
 
-function clear() {
+function clearTasksClick() {
+    remoteTasks();
+    if(tasksArray.length > 0)
+        tasksArray.splice(0, tasksArray.length);
     localStorage.clear();
-    pageLoad();
+    getLocalStorage();
 }
